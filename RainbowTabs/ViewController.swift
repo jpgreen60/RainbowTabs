5834928015d68deff13d8e469ea41d17626ccc8d
JohnPGreen <johnpgreen60@gmail.com>
2018-11-15 01:04:12 -0500

    Initial Commit

---
+++ RainbowTabs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by John Green on -11-152018.
+//  Copyright © 2018 John Green. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
